463a7594085ae72a6e8095edea4b4e162683c985
Sahil <36700901+SahilParikh03@users.noreply.github.com>
2025-11-19 15:16:51 +0100

    Add Escrow contract with state variables

---
+++ test-contracts/0_before/01_simple_storage_vars_2Blocks.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Escrow {
+    uint public arbiter;
+    address public beneficiary;
+    uint public depositor;
+    bytes32 public data;
+
+    uint public shafu;
+    uint  public shafu3;
+}
